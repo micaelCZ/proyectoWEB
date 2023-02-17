d96c36bd7090d6c890340c4a663f08fa5b1c25e7
Jhois Cedeño <jmcedeno10@espe.edu.ec>
2023-02-16 23:38:54 -0500

    Interación 1

---
+++ js/fechaHora.js
@@ -0,0 +1,36 @@
+
+     // Obtener el elemento con id "fecha-hora"**
+     var contenedor = document.getElementById("fecha-hora");
+
+     // Obtener el elemento hijo con etiqueta "time"**
+     var tiempo = contenedor.querySelector("time");
+
+     // Crear una función que actualice la fecha y hora cada segundo**
+     function actualizarFechaHora() {
+
+       // Crear un objeto de fecha con la fecha y hora actuales**
+       var fecha = new Date();
+
+       // Obtener los componentes de la fecha y hora**
+       var año = fecha.getFullYear();
+       var mes = (fecha.getMonth() + 1).toString().padStart(2, "0"); // Los meses empiezan en 0**
+       var dia = fecha.getDate().toString().padStart(2, "0");
+       var hora = fecha.getHours().toString().padStart(2, "0");
+       var minuto = fecha.getMinutes().toString().padStart(2, "0");
+       var segundo = fecha.getSeconds().toString().padStart(2, "0");
+
+       // Formatear la fecha y hora según el estándar ISO 8601 (YYYY-MM-DDThh:mm:ss)**
+       var formatoISO = año + "-" + mes + "-" + dia + "T" + hora + ":" + minuto + ":" + segundo;
+
+       // Escribir la fecha y hora en el atributo datetime del elemento time
+       tiempo.setAttribute("datetime", formatoISO);
+
+       // Escribir la fecha y hora en el contenido del elemento time
+       tiempo.innerHTML = formatoISO;
+     }
+
+     // Llamar a la función una vez al cargar la página
+     actualizarFechaHora();
+
+     // Llamar a la función cada segundo usando setInterval
+     setInterval(actualizarFechaHora, 1000);
---
+++ php/form1.php
@@ -0,0 +1,6 @@
+<?php
+      //$nombre = $_POST['nombre'];
+      //$cantidad = $_POST['cantidad'];
+      //$unidad = $_POST['unidad'];
+      //$fecha_vencimiento = $_POST['fecha_vencimiento'];
+?>
