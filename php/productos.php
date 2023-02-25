db89b9974350ed097c4de6c0258e348a814e926a
Jhois Cedeño <jmcedeno10@espe.edu.ec>
2023-02-25 13:58:21 -0500

    Arreglo archivos JSON. Diseño de formulario productos.

--- php/productos.php
+++ php/productos.php
@@ -1,26 +1,27 @@
 <?php
 try {
-  // Conexión a la base de datos utilizando PDO
-  $db = new PDO('sqlite:../db/materiaPrima.db');
-
-  // Obtener todos los productos de la tabla
-  $stmt = $db->query('SELECT * FROM productos');
-  $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-  // Convertir los datos a formato JSON
-  $json = json_encode($productos);
-
-  // Guardar los datos en un archivo JSON
-  file_put_contents('../json/productos.json', $json);
-
-  // Cerrar la conexión a la base de datos
-  $db = null;
-
-  //Volver a la página de productos
-    header('Location: ../html/productos.html');
-
-} catch (PDOException $e) {
-  // Mostrar un mensaje de error si hay un problema con la conexión a la base de datos
-  echo "Error al conectar a la base de datos: " . $e->getMessage();
-}
+    // Conexión a la base de datos utilizando PDO
+    $db = new PDO('sqlite:../db/materiaPrima.db');
+  
+    // Obtener todos los productos de la tabla
+    $stmt = $db->query('SELECT * FROM productos');
+    $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+    // Convertir los datos a formato JSON con la opción JSON_PRETTY_PRINT
+    $json = json_encode($productos, JSON_PRETTY_PRINT);
+  
+    // Guardar los datos en un archivo JSON
+    file_put_contents('../json/productos.json', $json);
+  
+    // Cerrar la conexión a la base de datos
+    $db = null;
+  
+    //Volver a la página de productos
+      header('Location: ../html/productos.html');
+  
+  } catch (PDOException $e) {
+    // Mostrar un mensaje de error si hay un problema con la conexión a la base de datos
+    echo "Error al conectar a la base de datos: " . $e->getMessage();
+  }
+  
 ?>
