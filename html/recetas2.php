877f24b79575927725a64416d226cc3c34af81e5
Katherine2001S <107556558+Katherine2001S@users.noreply.github.com>
2023-03-05 23:31:01 -0500

    Modificacion recetas3

--- html/recetas2.php
+++ html/recetas2.php
@@ -23,13 +23,13 @@
   <br>
   <br>
   <div id="receta1">
-  <form action="../php/restarInventario.php" method="post">
+  <form action="../html/restarInventario2.php" method="post">
     <h2>Pastel de chocolate (16 porciones)</h2>
     <center><img src="../img/pastelchocolate.png" style="width: 40%;"></center>
     <br>
     <label for="cantidad">Información de la orden:</label>
     <input type="hidden" id="producto" name="producto" value="Pastel de Chocolate">
-    <input type="number" id="cantidad-pasteles" name="cantidad" min=0 required autofocus style="width: 170px;" placeholder="Cantidad de pasteles" onchange="calcularCantidadIngredientes()">
+    <input type="number" id="cantidad-pasteles" name="cantidad     " min=0 required autofocus style="width: 170px;" placeholder="Cantidad de pasteles" onchange="calcularCantidadIngredientes()">
     <input type="text" id="campo_codigo" name="codigo" placeholder="Ingrese un código" style="width: 360px;">
     <!-- Ingrediente 1 -->
     <label for="nombre">Ingredientes:</label>
@@ -52,26 +52,74 @@
       $db = null;
       ?>
     </select>
-    <input type="number" id="cantidad1" name="cantidad" style="width: 300px;" placeholder="Cantidad" value="3" readonly>
-    <select name="unidad" id="unidad1" style="width: 300px;" readonly disabled>
+    <input type="number" id="cantidad1" name="cantidad" style="width: 80px;" placeholder="Cantidad" value="300" readonly>
+    <select name="unidad" id="unidad1" style="width: 80px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener las unidades de los productos
+      $stmt = $db->prepare('SELECT unidad FROM productos WHERE unidad = "gr"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['unidad'] . '">' . $row['unidad'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
     <br>
     <br>
     
-    <select name="nombre" id="nombre2" style="width: 300px;" value="Harina" placeholder="Cantidad" readonly disabled>
+
+
+
+
+    <select name="nombre" id="nombre2" style="width: 300px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener los nombres de los productos
+      $stmt = $db->prepare('SELECT nombre FROM productos WHERE nombre = "Mantequilla"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['nombre'] . '">' . $row['nombre'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
-    <input type="number" id="cantidad2" name="cantidad" style="width: 300px;" placeholder="Cantidad" value="3" readonly>
-    <select name="unidad" id="unidad2" style="width: 300px;" readonly disabled>
+    <input type="number" id="cantidad2" name="cantidad" style="width: 80px;" placeholder="Cantidad" value="90" readonly>
+    <select name="unidad" id="unidad2" style="width: 80px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener las unidades de los productos
+      $stmt = $db->prepare('SELECT unidad FROM productos WHERE unidad = "gr"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['unidad'] . '">' . $row['unidad'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
     <br>
     <br>
+
+
+
+
+
 
     <select name="nombre" id="nombre3" style="width: 300px;" readonly disabled>
       <?php
       // Conexión a la base de datos utilizando PDO
       $db = new PDO('sqlite:../db/materiaPrima.db');
       // Consulta para obtener los nombres de los productos
-      $stmt = $db->prepare('SELECT nombre FROM productos');
+      $stmt = $db->prepare('SELECT nombre FROM productos WHERE nombre = "Leche Condensada"');
       $stmt->execute();
       // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -81,18 +129,36 @@
       $db = null;
       ?>
     </select>
-    <input type="number" id="cantidad3" name="cantidad" style="width: 300px;" placeholder="Cantidad" value="3" readonly>
-    <select name="unidad" id="unidad3" style="width: 300px;" readonly disabled>
+    <input type="number" id="cantidad3" name="cantidad" style="width: 80px;" placeholder="Cantidad" value="387" readonly>
+    <select name="unidad" id="unidad3" style="width: 80px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener las unidades de los productos
+      $stmt = $db->prepare('SELECT unidad FROM productos WHERE unidad = "gr"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['unidad'] . '">' . $row['unidad'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
     <br>
     <br>
+
+
+
+
+
 
     <select name="nombre" id="nombre4" style="width: 300px;" readonly disabled>
       <?php
       // Conexión a la base de datos utilizando PDO
       $db = new PDO('sqlite:../db/materiaPrima.db');
       // Consulta para obtener los nombres de los productos
-      $stmt = $db->prepare('SELECT nombre FROM productos');
+      $stmt = $db->prepare('SELECT nombre FROM productos WHERE nombre = "Huevos"');
       $stmt->execute();
       // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -102,11 +168,27 @@
       $db = null;
       ?>
     </select>
-    <input type="number" id="cantidad4" name="cantidad" style="width: 300px;" placeholder="Cantidad" value="3" readonly>
-    <select name="unidad" id="unidad4" style="width: 300px;" readonly disabled>
+    <input type="number" id="cantidad4" name="cantidad" style="width: 80px;" placeholder="Cantidad" value="3" readonly>
+    <select name="unidad" id="unidad4" style="width: 80px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener las unidades de los productos
+      $stmt = $db->prepare('SELECT unidad FROM productos WHERE unidad = "gr"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['unidad'] . '">' . $row['unidad'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
     <br>
     <br>
+
+
+
 
 
     <select name="nombre" id="nombre5" style="width: 300px;" readonly disabled>
@@ -114,7 +196,7 @@
       // Conexión a la base de datos utilizando PDO
       $db = new PDO('sqlite:../db/materiaPrima.db');
       // Consulta para obtener los nombres de los productos
-      $stmt = $db->prepare('SELECT nombre FROM productos');
+      $stmt = $db->prepare('SELECT nombre FROM productos WHERE nombre = "Harina de Trigo"');
       $stmt->execute();
       // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -124,18 +206,36 @@
       $db = null;
       ?>
     </select>
-    <input type="number" id="cantidad5" name="cantidad" style="width: 300px;" placeholder="Cantidad" value="3" readonly>
-    <select name="unidad" id="unidad5" style="width: 300px;" readonly disabled>
+    <input type="number" id="cantidad5" name="cantidad" style="width: 80px;" placeholder="Cantidad" value="454" readonly>
+    <select name="unidad" id="unidad5" style="width: 80px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener las unidades de los productos
+      $stmt = $db->prepare('SELECT unidad FROM productos WHERE unidad = "gr"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['unidad'] . '">' . $row['unidad'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
     <br>
     <br>
+
+
+
+
+
 
     <select name="nombre" id="nombre6" style="width: 300px;" readonly disabled>
       <?php
       // Conexión a la base de datos utilizando PDO
       $db = new PDO('sqlite:../db/materiaPrima.db');
       // Consulta para obtener los nombres de los productos
-      $stmt = $db->prepare('SELECT nombre FROM productos');
+      $stmt = $db->prepare('SELECT nombre FROM productos WHERE nombre = "Levadura"');
       $stmt->execute();
       // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -145,8 +245,21 @@
       $db = null;
       ?>
     </select>
-    <input type="number" id="cantidad6" name="cantidad" style="width: 300px;" placeholder="Cantidad" value="3" readonly>
-    <select name="unidad" id="unidad6" style="width: 300px;" readonly disabled>
+    <input type="number" id="cantidad6" name="cantidad" style="width: 80px;" placeholder="Cantidad" value="10" readonly>
+    <select name="unidad" id="unidad6" style="width: 80px;" readonly disabled>
+      <?php
+      // Conexión a la base de datos utilizando PDO
+      $db = new PDO('sqlite:../db/materiaPrima.db');
+      // Consulta para obtener las unidades de los productos
+      $stmt = $db->prepare('SELECT unidad FROM productos WHERE unidad = "gr"');
+      $stmt->execute();
+      // Recorrer los resultados de la consulta y crear una opción en la lista desplegable para cada producto
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<option value="' . $row['unidad'] . '">' . $row['unidad'] . '</option>';
+      }
+      // Cerrar la conexión a la base de datos
+      $db = null;
+      ?>
     </select>
     <br>
     <br>
@@ -156,12 +269,12 @@
     <script>
     function calcularCantidadIngredientes() {
         const cantidadPasteles = document.getElementById("cantidad-pasteles").value;
-        document.getElementById("cantidad1").value = cantidadPasteles *3;
-        document.getElementById("cantidad2").value = cantidadPasteles *5;
-        document.getElementById("cantidad3").value = cantidadPasteles *2;
-        document.getElementById("cantidad4").value = cantidadPasteles *4;
-        document.getElementById("cantidad5").value = cantidadPasteles *6;
-        document.getElementById("cantidad6").value = cantidadPasteles *3;
+        document.getElementById("cantidad1").value = cantidadPasteles *300;
+        document.getElementById("cantidad2").value = cantidadPasteles *90;
+        document.getElementById("cantidad3").value = cantidadPasteles *387;
+        document.getElementById("cantidad4").value = cantidadPasteles *3;
+        document.getElementById("cantidad5").value = cantidadPasteles *454;
+        document.getElementById("cantidad6").value = cantidadPasteles *10;
     }
     </script>
 
---
+++ html/restarInventario2.php
@@ -0,0 +1,144 @@
+<?php
+// Conexión a la base de datos utilizando PDO
+$db = new PDO('sqlite:../db/materiaPrima.db');
+
+// Obtener la cantidad de chocolate disponible en la base de datos
+$stmt = $db->prepare('SELECT cantidad FROM productos WHERE nombre = "Chocolate"');
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$cantidad_chocolate_disponible = $row['cantidad'];
+
+// Calcular la cantidad de chocolate necesaria para hacer los pasteles
+$cantidad_chocolate_necesario = $_POST['cantidad'] * 300;
+
+// Restar la cantidad de chocolate necesario de la cantidad de chocolate disponible
+$nueva_cantidad_chocolate = $cantidad_chocolate_disponible - $cantidad_chocolate_necesario;
+
+// Actualizar la cantidad de chocolate en la base de datos
+$stmt = $db->prepare('UPDATE productos SET cantidad = :nueva_cantidad_chocolate WHERE nombre = "Chocolate"');
+$stmt->bindParam(':nueva_cantidad_chocolate', $nueva_cantidad_chocolate, PDO::PARAM_INT);
+$stmt->execute();
+
+
+
+
+// Obtener la cantidad de mantequilla disponible en la base de datos
+$stmt = $db->prepare('SELECT cantidad FROM productos WHERE nombre = "Mantequilla"');
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$cantidad_mantequilla_disponible = $row['cantidad'];
+
+// Calcular la cantidad de mantequilla necesaria para hacer los pasteles
+$cantidad_mantequilla_necesario = $_POST['cantidad'] * 540;
+
+// Restar la cantidad de mantequilla necesario de la cantidad de mantequilla disponible
+$nueva_cantidad_mantequilla = $cantidad_mantequilla_disponible - $cantidad_mantequilla_necesario;
+
+// Actualizar la cantidad de mantequilla en la base de datos
+$stmt = $db->prepare('UPDATE productos SET cantidad = :nueva_cantidad_mantequilla WHERE nombre = "Mantequilla"');
+$stmt->bindParam(':nueva_cantidad_mantequilla', $nueva_cantidad_mantequilla, PDO::PARAM_INT);
+$stmt->execute();
+
+
+
+
+
+
+
+// Obtener la cantidad de Leche Condensada disponible en la base de datos
+$stmt = $db->prepare('SELECT cantidad FROM productos WHERE nombre = "Leche Condensada"');
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$cantidad_lechecondensada_disponible = $row['cantidad'];
+
+// Calcular la cantidad de Leche Condensada necesaria para hacer los pasteles
+$cantidad_lechecondensada_necesario = $_POST['cantidad'] * 540;
+
+// Restar la cantidad de Leche Condensada necesario de la cantidad de Leche Condensada disponible
+$nueva_cantidad_lechecondensada = $cantidad_lechecondensada_disponible - $cantidad_lechecondensada_necesario;
+
+// Actualizar la cantidad de Leche Condensada en la base de datos
+$stmt = $db->prepare('UPDATE productos SET cantidad = :nueva_cantidad_lechecondensada WHERE nombre = "Leche Condensada"');
+$stmt->bindParam(':nueva_cantidad_lechecondensada', $nueva_cantidad_lechecondensada, PDO::PARAM_INT);
+$stmt->execute();
+
+
+
+
+
+
+
+
+
+
+// Obtener la cantidad de Huevos disponible en la base de datos
+$stmt = $db->prepare('SELECT cantidad FROM productos WHERE nombre = "Huevos"');
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$cantidad_Huevos_disponible = $row['cantidad'];
+
+// Calcular la cantidad de Huevos necesaria para hacer los pasteles
+$cantidad_Huevos_necesario = $_POST['cantidad'] * 540;
+
+// Restar la cantidad de Huevos necesario de la cantidad de Huevos disponible
+$nueva_cantidad_Huevos = $cantidad_Huevos_disponible - $cantidad_Huevos_necesario;
+
+// Actualizar la cantidad de Huevos en la base de datos
+$stmt = $db->prepare('UPDATE productos SET cantidad = :nueva_cantidad_Huevos WHERE nombre = "Huevos"');
+$stmt->bindParam(':nueva_cantidad_Huevos', $nueva_cantidad_Huevos, PDO::PARAM_INT);
+$stmt->execute();
+
+
+
+
+
+
+
+
+// Obtener la cantidad de Harina de Trigo disponible en la base de datos
+$stmt = $db->prepare('SELECT cantidad FROM productos WHERE nombre = "Harina de Trigo"');
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$cantidad_HarinadeTrigo_disponible = $row['cantidad'];
+
+// Calcular la cantidad de Harina de Trigo necesaria para hacer los pasteles
+$cantidad_HarinadeTrigo_necesario = $_POST['cantidad'] * 540;
+
+// Restar la cantidad de Harina de Trigo necesario de la cantidad de Harina de Trigo disponible
+$nueva_cantidad_HarinadeTrigo = $cantidad_HarinadeTrigo_disponible - $cantidad_HarinadeTrigo_necesario;
+
+// Actualizar la cantidad de Harina de Trigo en la base de datos
+$stmt = $db->prepare('UPDATE productos SET cantidad = :nueva_cantidad_HarinadeTrigo WHERE nombre = "Harina de Trigo"');
+$stmt->bindParam(':nueva_cantidad_HarinadeTrigo', $nueva_cantidad_HarinadeTrigo, PDO::PARAM_INT);
+$stmt->execute();
+
+
+
+
+
+
+// Obtener la cantidad de Levadura disponible en la base de datos
+$stmt = $db->prepare('SELECT cantidad FROM productos WHERE nombre = "Levadura"');
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$cantidad_Levadura_disponible = $row['cantidad'];
+
+// Calcular la cantidad de Levadura necesaria para hacer los pasteles
+$cantidad_Levadura_necesario = $_POST['cantidad'] * 540;
+
+// Restar la cantidad de Levadura necesario de la cantidad de Levaduradisponible
+$nueva_cantidad_Levadura = $cantidad_Levadura_disponible - $cantidad_Levadura_necesario;
+
+// Actualizar la cantidad de Levadura en la base de datos
+$stmt = $db->prepare('UPDATE productos SET cantidad = :nueva_cantidad_Levadura WHERE nombre = "Levadura"');
+$stmt->bindParam(':nueva_cantidad_Levadura', $nueva_cantidad_Levadura, PDO::PARAM_INT);
+$stmt->execute();
+
+
+
+
+
+
+// Cerrar la conexión con la base de datos
+$db = null;
+?>
